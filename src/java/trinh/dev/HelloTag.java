8f0bb5336d3696f9c1f23842561d1b0a1c9ffd75
thanhtrinh1532 <thanhtrinh15032000@gmail.com>
2024-10-10 00:35:54 +0700

    version1

---
+++ src/java/trinh/dev/HelloTag.java
@@ -0,0 +1,17 @@
+package trinh.dev;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import jakarta.servlet.jsp.tagext.*;
+import jakarta.servlet.jsp.*;
+import java.io.*;
+
+public class HelloTag extends SimpleTagSupport {
+   public void doTag() throws JspException, IOException {
+      JspWriter out = getJspContext().getOut();
+      out.println("Hello Custom Tag!");
+   }
+}
